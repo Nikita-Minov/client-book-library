de9b361850dc5b66556874ae366c31deac30ea07
Nikita-Minov <nikitachryackhov555@gmail.com>
2021-08-15 11:19:56 +0300

    The client side is prepared for deployment to Heroku

--- src/api/usersAPI.js
+++ src/api/usersAPI.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'https://mern-library-app-server.herokuapp.com/api',
+    baseURL: 'http://localhost:3001/api',
     withCredentials: true
 });
 
